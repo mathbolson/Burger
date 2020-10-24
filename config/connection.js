a07801975464d1012b5af3c8b1a7a6f7d72fcaaa
Matheus Bolson <mathbolson@hotmail.com>
2020-10-23 21:43:26 -0700

    new changes

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,21 @@
-// Set up MySQL connection.
 var mysql = require("mysql");
 
-if (process.env.JAWSDS_URL) {
-  connection = mysql.createConnection(process.env.JAWSDS_URL)
-} else {
-  connection = mysql.createConnection({
-    host: "localhost",
+// Sets up the Express app to handle data parsing
+
+var connection = mysql.createConnection({
+  host: "localhost",
   port: 3306,
   user: "root",
   password: "root",
   database: "burgers_db"
-  });
-}
+});
 
-
-
-// Make connection.
-connection.connect();
-
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+}); 
 // Export connection for our ORM to use.
 module.exports = connection;
